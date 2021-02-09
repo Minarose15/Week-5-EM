e9e612b7aee583b3fa48555e03da09a90eba5378
Minarose15 <76829050+Minarose15@users.noreply.github.com>
2021-02-09 12:35:37 -0700

    finished code

--- ReportWeek5.py
+++ ReportWeek5.py
@@ -15,8 +15,8 @@ class Capacitor:
         self.d = d
         self.L = L
         self.Voltage = zeros([self.L,self.L], float)
-        self.Voltage[int(self.L/2-self.L/6):int(self.L+self.L/6)+1, 
-                     int(self.L-self.d*self.L/4)] = 1
+        self.Voltage[int(self.L/2-self.L/6):int(self.L/2+self.L/6)+1, 
+                     int(self.L/2-self.d*self.L/4)] = 1
         self.Voltage[int(L/2-L/6):int(L/2+L/6)+1, int(L/2+self.d*L/4)]= -1
         
     def SOR(self):
@@ -44,28 +44,22 @@ class Capacitor:
         imshow(self.Voltage)
         plasma()
         show()
-        
-    def ElectricField(self):
-        return gradient(self.Voltage)
     
     def electricPlot(self):
         self.SOR()
-
-        x = linspace(0,self.L,self.L)        #makes ranges to loop through, 150 points in each
+        x = linspace(0,self.L,self.L)        
         y = linspace(0,self.L,self.L)
         X,Y = meshgrid(x,y)
-        dX, dY = gradient(transpose(self.Voltage)0)
-        quiver(X, Y, dX, dY, color = "k")
+        dY, dX = gradient(self.Voltage)
+        quiver(X, Y, -dX, -dY, color = "k")
         axes().set_aspect('equal')
         show()
 
 capac8 = Capacitor(0.8,50)
-#capac8.voltagePlot()
 
 capac3 = Capacitor(0.3,50)
-#capac3.voltagePlot()
 
 capac10 = Capacitor(1.0, 50)
-capac8.electricPlot()
 capac3.electricPlot()
+capac8.electricPlot()
 capac10.electricPlot()
